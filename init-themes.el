7c5b2567364c8c73ac4f8aa7aac322dfa9af0b60
Linghui Chen <clh329@gmail.com>
2012-05-22 17:19:21 +0800

    change color theme

--- init-themes.el
+++ init-themes.el
@@ -1,74 +1,77 @@
 ;;------------------------------------------------------------------------------
 ;; Old-style color theming support (via color-theme.el)
 ;;------------------------------------------------------------------------------
-(defcustom window-system-color-theme 'color-theme-sanityinc-solarized-dark
-  "Color theme to use in window-system frames.
-If Emacs' native theme support is available, this setting is
-ignored: use `custom-enabled-themes' instead."
-  :type 'symbol)
+(require 'color-theme-sanityinc-tomorrow)
+(color-theme-sanityinc-tomorrow-eighties)
 
-(defcustom tty-color-theme 'color-theme-terminal
-  "Color theme to use in TTY frames.
-If Emacs' native theme support is available, this setting is
-ignored: use `custom-enabled-themes' instead."
-  :type 'symbol)
+;; (defcustom window-system-color-theme 'color-theme-sanityinc-solarized-dark
+;;   "Color theme to use in window-system frames.
+;; If Emacs' native theme support is available, this setting is
+;; ignored: use `custom-enabled-themes' instead."
+;;   :type 'symbol)
 
-(unless (boundp 'custom-enabled-themes)
-  (defun color-theme-terminal ()
-    (interactive)
-    (color-theme-sanityinc-solarized-dark))
+;; (defcustom tty-color-theme 'color-theme-terminal
+;;   "Color theme to use in TTY frames.
+;; If Emacs' native theme support is available, this setting is
+;; ignored: use `custom-enabled-themes' instead."
+;;   :type 'symbol)
 
-  (defun apply-best-color-theme-for-frame-type (frame)
-    (with-selected-frame frame
-      (funcall (if window-system
-                   window-system-color-theme
-                 tty-color-theme))))
+;; (unless (boundp 'custom-enabled-themes)
+;;   (defun color-theme-terminal ()
+;;     (interactive)
+;;     (color-theme-sanityinc-solarized-dark))
 
-  (defun reapply-color-themes ()
-    (interactive)
-    (mapcar 'apply-best-color-theme-for-frame-type (frame-list)))
+;;   (defun apply-best-color-theme-for-frame-type (frame)
+;;     (with-selected-frame frame
+;;       (funcall (if window-system
+;;                    window-system-color-theme
+;;                  tty-color-theme))))
 
-  (set-variable 'color-theme-is-global nil)
-  (add-hook 'after-make-frame-functions 'apply-best-color-theme-for-frame-type)
-  (add-hook 'after-init-hook 'reapply-color-themes)
-  (apply-best-color-theme-for-frame-type (selected-frame)))
+;;   (defun reapply-color-themes ()
+;;     (interactive)
+;;     (mapcar 'apply-best-color-theme-for-frame-type (frame-list)))
 
-
-;;------------------------------------------------------------------------------
-;; New-style theme support, in which per-frame theming is not possible
-;;------------------------------------------------------------------------------
-
-;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-solarized-light))
-
-;; Ensure that themes will be applied even if they have not been customized
-(defun reapply-themes ()
-  "Forcibly load the themes listed in `custom-enabled-themes'."
-  (dolist (theme custom-enabled-themes)
-    (unless (custom-theme-p theme)
-      (load-theme theme)
-      (message "Consider using 'M-x customize-themes' to save your preferred theme.")))
-  (custom-set-variables `(custom-enabled-themes ,custom-enabled-themes)))
-
-(add-hook 'after-init-hook 'reapply-themes)
+;;   (set-variable 'color-theme-is-global nil)
+;;   (add-hook 'after-make-frame-functions 'apply-best-color-theme-for-frame-type)
+;;   (add-hook 'after-init-hook 'reapply-color-themes)
+;;   (apply-best-color-theme-for-frame-type (selected-frame)))
 
 
-;;------------------------------------------------------------------------------
-;; Toggle between light and dark
-;;------------------------------------------------------------------------------
-(defun light ()
-  "Activate a light color theme."
-  (interactive)
-  (if (boundp 'custom-enabled-themes)
-      (custom-set-variables '(custom-enabled-themes '(sanityinc-solarized-light)))
-    (color-theme-sanityinc-solarized-light)))
+;; ;;------------------------------------------------------------------------------
+;; ;; New-style theme support, in which per-frame theming is not possible
+;; ;;------------------------------------------------------------------------------
 
-(defun dark ()
-  "Activate a dark color theme."
-  (interactive)
-  (if (boundp 'custom-enabled-themes)
-      (custom-set-variables '(custom-enabled-themes '(sanityinc-solarized-dark)))
-    (color-theme-sanityinc-solarized-dark)))
+;; ;; If you don't customize it, this is the theme you get.
+;; (setq-default custom-enabled-themes '(sanityinc-solarized-light))
+
+;; ;; Ensure that themes will be applied even if they have not been customized
+;; (defun reapply-themes ()
+;;   "Forcibly load the themes listed in `custom-enabled-themes'."
+;;   (dolist (theme custom-enabled-themes)
+;;     (unless (custom-theme-p theme)
+;;       (load-theme theme)
+;;       (message "Consider using 'M-x customize-themes' to save your preferred theme.")))
+;;   (custom-set-variables `(custom-enabled-themes ,custom-enabled-themes)))
+
+;; (add-hook 'after-init-hook 'reapply-themes)
+
+
+;; ;;------------------------------------------------------------------------------
+;; ;; Toggle between light and dark
+;; ;;------------------------------------------------------------------------------
+;; (defun light ()
+;;   "Activate a light color theme."
+;;   (interactive)
+;;   (if (boundp 'custom-enabled-themes)
+;;       (custom-set-variables '(custom-enabled-themes '(sanityinc-solarized-light)))
+;;     (color-theme-sanityinc-solarized-light)))
+
+;; (defun dark ()
+;;   "Activate a dark color theme."
+;;   (interactive)
+;;   (if (boundp 'custom-enabled-themes)
+;;       (custom-set-variables '(custom-enabled-themes '(sanityinc-solarized-dark)))
+;;     (color-theme-sanityinc-solarized-dark)))
 
 
 (provide 'init-themes)
